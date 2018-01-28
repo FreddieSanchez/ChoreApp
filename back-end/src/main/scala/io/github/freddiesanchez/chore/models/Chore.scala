da81c7fac7e9a5a2104bec8afe07109fbab33414
Freddie Sanchez <freddie.sanchez@gmail.com>
2018-01-28 11:56:42 -0700

    more refactoring

--- back-end/src/main/scala/io/github/freddiesanchez/chore/models/Chore.scala
+++ back-end/src/main/scala/io/github/freddiesanchez/chore/models/Chore.scala
@@ -1,21 +1,5 @@
 package io.github.freddiesanchez.chore.models
 
-import io.circe._
-import io.circe.syntax._
-import io.circe.generic.auto._
-import org.http4s.circe._
-import io.circe.generic.semiauto._
-
 case class ChoreInput(name:String, description:String, rating: Rating)
-
-object ChoreIntput {
-    implicit val ChoreEncoder: Encoder[ChoreInput] = deriveEncoder[ChoreInput]
-    implicit val ChoreDecoder: Decoder[ChoreInput] = deriveDecoder[ChoreInput]
-}
-
 case class Chore(id: Int, name:String, description: String, rating: Rating)
 
-object Chore {
-    implicit val ChoreEncoder: Encoder[Chore] = deriveEncoder[Chore]
-    implicit val ChoreDecoder: Decoder[Chore] = deriveDecoder[Chore]
-}
---
+++ back-end/src/main/scala/io/github/freddiesanchez/chore/models/Json.scala
@@ -0,0 +1,18 @@
+package io.github.freddiesanchez.chore.models
+
+import io.circe._
+import io.circe.syntax._
+import io.circe.generic.auto._
+import org.http4s.circe._
+import io.circe.generic.semiauto._
+
+object Json {
+    implicit val ChoreInputEncoder: Encoder[ChoreInput] = deriveEncoder[ChoreInput]
+    implicit val ChoreIntputDecoder: Decoder[ChoreInput] = deriveDecoder[ChoreInput]
+
+    implicit val ChoreEncoder: Encoder[Chore] = deriveEncoder[Chore]
+    implicit val ChoreDecoder: Decoder[Chore] = deriveDecoder[Chore]
+
+    implicit val RatingEncoder: Encoder[Rating] = deriveEncoder[Rating]
+    implicit val RatingDecorder: Decoder[Rating] = deriveDecoder[Rating]
+}
